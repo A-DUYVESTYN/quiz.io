699b0f1025289b1883e8f7900d15300605bce3b0
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-30 19:26:47 -0400

    updated DB tables

---
+++ db/schema/04_attempts.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS attempts CASCADE;
+CREATE TABLE attempts (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES users(id),
+    quiz_id INTEGER REFERENCES quizzes(id),
+    url VARCHAR(255),
+    date_attempted TIMESTAMP,
+    score INTEGER
+);
