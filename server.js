c522954429427dd763a623f8b56dcec0ef4bca43
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-07-30 22:30:39 +0000

    start quiz_show.js route

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const quiz_showRoutes = require("./routes/quiz_show");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/quiz_show", quiz_showRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
