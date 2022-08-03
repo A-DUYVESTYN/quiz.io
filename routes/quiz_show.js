ce985d4e1a572a808d5a9923039616427c835b65
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-08-03 01:38:26 +0000

    add Attempt URL generator

--- routes/quiz_show.js
+++ routes/quiz_show.js
@@ -9,6 +9,7 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+  // GET ROUTES ////////////////////////////////////////////////
   // temporary "/api/quiz_show/" page for development
   router.get("/", (req, res) => {
     res.send("enter the url for a quiz. for example /api/quiz_show/8u8u8u")
@@ -36,6 +37,18 @@ module.exports = (db) => {
       });
   });
 
+  // FUNCTIONS /////////////////////////////////////////////////
+
+  const generateRandomString = function() {
+    let length = 6;
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  };
+
   const getQuizID = function (quizURL) {
     return db.query(`
     SELECT id from quizzes
@@ -50,6 +63,7 @@ module.exports = (db) => {
   }
 
   const addAttempt = function (userId, quizId, attemptUrl) {
+    // INSERT a row in the "attempts" table with columns: user_id, quiz_id, url (for sharing the quiz attempt score), and date_attempted.
     const dateOfAttempt = new Date();
     return db
       .query(`INSERT INTO attempts (user_id, quiz_id, url, date_attempted)
@@ -62,7 +76,7 @@ module.exports = (db) => {
   }
 
   const addAttemptScores = function (quizAttempt, answers) {
-    // SELECT query to get the questionAndAnswer_id for each question, then compare user's guess to the correct answer and INSERT all the results in attempt_scores table.
+    // SELECT query to get the questionAndAnswer_id for each question, then compare user's guess to the correct answer
     return db
       .query(`SELECT * FROM questionsAndAnswer
     WHERE quiz_id = $1;`, [quizAttempt.quiz_id])
@@ -90,7 +104,7 @@ module.exports = (db) => {
         console.log("scores array for INSERT query:", scores)
         return scores
       })
-      // INSERT INTO query for each question the user answered to store "attempts_id, questionAndAnswer_id, user_guess, and correct"
+      // INSERT INTO query creates a row for each question of the quiz to store "attempts_id, questionAndAnswer_id, user_guess, and correct" columns
       .then(res => {
         const values = res.map(item => [item.attempts_id, item.questionsandanswer_id, item.user_guess, item.correct])
         console.log(values)
@@ -106,22 +120,28 @@ module.exports = (db) => {
       })
   }
 
-  // this POST needs to create a row in the "attempts" table with columns: user_id, quiz_id, url (of the attempt, for sharing), and date_attempted.
-  // Then the user's answers should be inserted into the attempt_scores table with columns: attempts_id, questionAndAnswer_id, user_guess, and correct) Need to verify if the answer is correct before passing boolean to 'correct' column
+  // POST ROUTES //////////////////////////////////////////////
+
+  // determine correct/incorrect and store results in "attempts" and "attempt_scores" db tables
   // then redirect to the score page (using attempts.url)
   router.post("/:id", (req, res) => {
-    console.log(req.body) // log user's answers (array)
 
-    const quizUrl = req.params.id
-    console.log(quizUrl)
-    // faked data for coding functions before login/generateQuizAttemptURL/ is coded
     const userAnswers = req.body["quiz-answer"]
+    const quizUrl = req.params.id
+    const attemptUrl = generateRandomString();
+
+    console.log(req.body) // log user's answers (array)
+    console.log(quizUrl)
+
+    // faked data for coding functions before login is coded
     const fakedUserID = 3;
-    const fakedAttemptURL = 'dfdfdf';
+    console.log(req.session.userId);
+
+    // const fakedAttemptURL = 'dfdfdf';
 
     getQuizID(quizUrl)
       .then(quizID => {
-        return addAttempt(fakedUserID, quizID, fakedAttemptURL)
+        return addAttempt(fakedUserID, quizID, attemptUrl)
       })
       .then(attempt => {
         if (!attempt) {
